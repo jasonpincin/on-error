b32cd5889a411207eec8004b231502993036689b
Jason Pincin <jason@pincin.com>
2015-07-22 08:16:07 -0400

    de-nested tests

--- test/emit.js
+++ test/emit.js
@@ -8,30 +8,30 @@ test('on-error.emit', function (t) {
     t.equal(typeof onError.emit, 'function', 'should be a function')
     t.throws(onError.emit, 'should require an emitter')
     t.ok(typeof onError.emit(emitter) === 'function', 'should return a function')
-
-    test('when passed an error', function (t) {
-        var emitter = new EventEmitter,
-            emits   = []
-        emitter.on('error', function (err) {
-            emits.push(err)
-        })
-        var anError = new Error('error 1')
-        t.notOk(onError.emit(emitter)(anError), 'should return undefined')
-        t.equal(emits.length, 1, 'should emit')
-        t.equal(emits[0], anError, 'should emit given error')
-        t.end()
-    })
-
-    test('when passed no error', function (t) {
-        var emitter = new EventEmitter,
-            emits   = []
-        emitter.on('error', function (err) {
-            emits.push(err)
-        })
-        t.notOk(onError.emit(emitter)(null), 'should return undefined')
-        t.equal(emits.length, 0, 'should not emit')
-        t.end()
-    })
-
     t.end()
 })
+
+test('on-error.emit when passed an error', function (t) {
+    var emitter = new EventEmitter,
+    emits   = []
+    emitter.on('error', function (err) {
+        emits.push(err)
+    })
+    var anError = new Error('error 1')
+    t.notOk(onError.emit(emitter)(anError), 'should return undefined')
+    t.equal(emits.length, 1, 'should emit')
+    t.equal(emits[0], anError, 'should emit given error')
+    t.end()
+})
+
+test('on-error.emit when passed no error', function (t) {
+    var emitter = new EventEmitter,
+    emits   = []
+    emitter.on('error', function (err) {
+        emits.push(err)
+    })
+    t.notOk(onError.emit(emitter)(null), 'should return undefined')
+    t.equal(emits.length, 0, 'should not emit')
+    t.end()
+})
+
--- test/on-error.js
+++ test/on-error.js
@@ -5,30 +5,30 @@ test('on-error', function (t) {
 
     t.equal(typeof onError, 'function', 'should be a function')
     t.throws(onError, 'requires a callback')
-
-    test('when passed an error', function (t) {
-        var errorCalls = []
-        function errHandler () {
-            var args = Array.prototype.slice.call(arguments, 0)
-            errorCalls.push(args)
-        }
-        var anError = new Error('error 1')
-        t.notOk(onError(errHandler)(anError), 'should return undefined')
-        t.equal(errorCalls.length, 1, 'should invoke error handler')
-        t.equal(errorCalls[0][0], anError, 'should pass error to handler')
-        t.end()
-    })
-
-    test('when passed no error', function (t) {
-        var errorCalls = []
-        function errHandler () {
-            var args = Array.prototype.slice.call(arguments, 0)
-            errorCalls.push(args)
-        }
-        t.notOk(onError(errHandler)(null), 'should return undefined')
-        t.equal(errorCalls.length, 0, 'should not invoke error handler')
-        t.end()
-    })
-
     t.end()
 })
+
+test('on-error when passed an error', function (t) {
+    var errorCalls = []
+    function errHandler () {
+        var args = Array.prototype.slice.call(arguments, 0)
+        errorCalls.push(args)
+    }
+    var anError = new Error('error 1')
+    t.notOk(onError(errHandler)(anError), 'should return undefined')
+    t.equal(errorCalls.length, 1, 'should invoke error handler')
+    t.equal(errorCalls[0][0], anError, 'should pass error to handler')
+    t.end()
+})
+
+test('on-error when passed no error', function (t) {
+    var errorCalls = []
+    function errHandler () {
+        var args = Array.prototype.slice.call(arguments, 0)
+        errorCalls.push(args)
+    }
+    t.notOk(onError(errHandler)(null), 'should return undefined')
+    t.equal(errorCalls.length, 0, 'should not invoke error handler')
+    t.end()
+})
+
